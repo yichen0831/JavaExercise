16d9bbd7c60d98bfa0d5a08a7523b8b516cbfae5
Yichen Huang <yichen0831@gmail.com>
2015-10-29 17:44:43 +0800

    Add findPIv2 to MonteCarloPI

--- src/practice/mathematics/MonteCarloPI.java
+++ src/practice/mathematics/MonteCarloPI.java
@@ -2,6 +2,26 @@ package practice.mathematics;
 
 public class MonteCarloPI {
 
+    public static double findPIv2(int number) {
+        double x;
+        double y;
+        int count = 0;
+        
+        double increament = 1 / (double) number;
+        
+        for (int i = 0; i < number; i++) {
+            x = increament * i;
+            for (int j = 0; j < number; j++) {
+                y = increament * j;
+                if (x * x + y * y < 1.0) {
+                    count++;
+                }
+            }
+        }
+
+        return (double) count * 4 / (number * number);
+    }
+
     public static double findPI(int number) {
 
         double x;
@@ -21,6 +41,7 @@ public class MonteCarloPI {
     }
 
     public static void main(String[] args) {
-        System.out.println("Pi is " + findPI(5000001));
+        System.out.println("V1 Pi is " + findPI(8000000));
+        System.out.println("V2 Pi is " + findPIv2(10000));
     }
 }
